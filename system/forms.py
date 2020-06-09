0fdc24dc3eb3d1c5f55d8728d8768761b310529d
AKAI <lsdintheworld@gmail.com>
2020-06-09 16:27:44 +0900

    No2

--- system/forms.py
+++ system/forms.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.forms import AuthenticationForm
+from site2.models import News
+from django import forms
+
+
 
 class LoginForm(AuthenticationForm):
     def __init__(self, request,  *args, **kwargs):
         super().__init__(request, *args, **kwargs)
 
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'  #←もしかしたら必要ないかも？bootstrapを利用しないため
+            field.widget.attrs['class'] = 'form-control'  
             field.widget.attrs['placeholder'] = field.label
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ('title', 'category', 'content', 'data')
+    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        CHOICE = ('お知らせ', '採用について', '製品情報', '株主の皆様へ')
+        self.fields['category'].choice=CHOICE
--- system/views.py
+++ system/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (LoginView, LogoutView)
-from .forms import LoginForm
+from .forms import LoginForm, NewsForm
+from django.contrib.auth.decorators import login_required
 
 class Login(LoginView):
     form_class = LoginForm
@@ -13,5 +14,11 @@ class Logout(LoginRequiredMixin, LogoutView):
     template_name = 'system/login.html'
 
 
+@login_required
 def index(request):
     return render(request, 'system/index.html')
+
+
+def news(request):
+    form = NewsForm()
+    return render(request, 'system/news.html', {'form': form})
