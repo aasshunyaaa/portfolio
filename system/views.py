27d5dc64ede70765b96eaed3a4a896ed94fec9bf
unknown <lsdintheworld@gmail.com>
2020-06-19 16:13:59 +0900

    kec6/19

--- system/views.py
+++ system/views.py
@@ -6,6 +6,8 @@ from .forms import LoginForm, NewsForm
 from django.contrib.auth.decorators import login_required
 from site2.models import News
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 class Login(LoginView):
     form_class = LoginForm
     template_name = 'system/login.html'
@@ -20,12 +22,25 @@ def index(request):
     return render(request, 'system/index.html')
 
 
+
+# ページネーションの関数
+def paginator_query(request, queryset, count):
+    paginator = Paginator(queryset, count)
+    page = request.GET.get('page')
+    try:
+        news_list = paginator.page(page)
+    except PageNotAnInteger:
+        news_list = paginator.page(1)
+    except EmptyPage:
+        news_list = paginator.page(paginator.num_pages)
+    return news_list
+
 # 新着情報の一覧
 @login_required
 def news(request):
     news = News.objects.all()
-    return render(request, 'system/news.html', {'news': news,})
-
+    news_list = paginator_query(request, news, 3)
+    return render(request, 'system/news.html', {'news': news_list.object_list, 'news_list': news_list,})
 
 # 新着情報の新規作成
 @login_required
